1c79abd8352203866b683d8500c45aca62c72529
Wwhds <a1605691832@163.com>
2024-07-10 16:07:46 +0800

    修改数值比较器，新增由二路选择器组成的四路选择器

--- equal_comparatortb.v
+++ equal_comparatortb.v
@@ -1,13 +1,39 @@
-module equal_comparator;
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/07/09 09:30:57
+// Design Name: 
+// Module Name: equal_comparatortb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module equal_comparatortb;
 
 // 输入
 reg a;
 reg b;
 
+// 输出
+wire out;
+
 equal_comparator eq(
-    .a(a)
-    .b(b)
-)
+    .a(a),
+    .b(b),
+    .out(out)
+);
 
 initial begin
     a = 0;b = 0;
@@ -15,9 +41,8 @@ initial begin
     a = 1;b = 0;
     #10
     a = 0;b = 1;
-    #
+    #10
     a = 1;b = 1;
-    $finish
+    $finish;
     end
 endmodule
-    
